a37a5766dbae1b2d17e0a1fb2372685668977b48
DESKTOP-GQ60U0C\student <241210@st.yoshida-g.ac.jp>
2026-07-02 10:16:26 +0900

    g

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ import { Game } from './game.js';
 window.addEventListener('DOMContentLoaded', () => {
     const game = new Game();
     window.game = game;
+    const loading = document.getElementById('loading-screen');
+    if (loading) {
+        loading.classList.add('hidden');
+    }
 });
